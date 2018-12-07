f7ebbeb182c13755a3b1dcd021f1d6244c7bd8d4
Shane Walsh <shaneneilwalsh@gmail.com>
2018-12-07 15:50:58 +0000

    adding party basic support

--- Server.js
+++ Server.js
@@ -16,6 +16,7 @@ const io = socketIO(server);
 //io.set('origins', 'http://localhost:* https://anotherdomain.com:*'); // Come back to this at a later date, when we want to restrcit access.
 
 let usernameTokenMap={};
+let partyIdTicker = 0;
 
 // Logging
 function logg(message, type="debug"){
@@ -38,7 +39,7 @@ function generateToken({}){
   return str;
 }
 
-var loginSocket = '/login'; // this is the socket for the chat box
+var loginSocket = '/login';
 var login = io
  .of(loginSocket)
  .on('connection', function (socket) { // on conection, outlines the calls the connect can make
@@ -51,9 +52,9 @@ var login = io
       let token = data.username + generateToken({});
       let playerData = getPlayerData({username: data.username});
       usernameTokenMap[''+data.username] = token;
-      login.emit('attemptLoginReply',{token:token,playerData:playerData}); // send a chat to everyone connected to chat on this game
+      socket.emit('attemptLoginReply',{token:token,playerData:playerData}); // send a replay to only the sender socket.
     } else { // login failed.
-      login.emit('attemptLoginReply',{error:true}); // send a chat to everyone connected to chat on this game
+      socket.emit('attemptLoginReply',{error:true}); // send a replay to only the sender socket.
     }
   });
 
@@ -93,7 +94,7 @@ setInterval(() => logLoggedInPlayers(), 10000);
 //####################################################################
 let tavernSockets={}; // we only need to track one of the sockets, once this diconnects, the user is no longer considered active on the server.
 
-var tavernChat = '/tavern'; // this is the socket for the chat box
+var tavernChat = '/tavern';
 var tavern = io
   .of(tavernChat)
   .on('connection', function (socket) { // on conection, outlines the calls the connect can make
@@ -103,8 +104,8 @@ var tavern = io
      tavernSockets[socket] = data.username;
    });
    socket.on('shout', function (data) {
-     // validate its from a logged in user, then shout it to every socket listening on this type.
-    let shoutBack = {username:data.username,shout:data.shout}; // remove token basically.
+     // todo validate its from a logged in user, then shout it to every socket listening on this type.
+    let shoutBack = {username:data.username,shout:data.shout};
     tavern.emit('shoutBack',shoutBack); // send a chat to everyone connected to chat on this game
    });
    socket.on('disconnect', (reason) => { // remove them from the logged in users.
@@ -121,10 +122,11 @@ var tavern = io
 
  let partySockets={}; // username to socket link.
  let parties = {}; // each party should have an id, {id:{id:id,name:name,members:[]}}
+// todo store socket to party links, when a user leaves, if he is the last member of that party, remove that party.
 
 // party id : 5 users 5 does event. broacast to all on party.
 
- var partyChat = '/party'; // this is the socket for the chat box
+ var partyChat = '/party';
  var party = io
    .of(partyChat)
    .on('connection', function (socket) { // on conection, outlines the calls the connect can make
@@ -135,22 +137,40 @@ var tavern = io
     socket.on('shout', function (data) {
       // validate its from a logged in user, then shout it to every socket listening on this type.
      let shoutBack = {username:data.username,shout:data.shout}; // remove token basically.
+     // todo loop on the member sockets of the group and send the shout to them all.
      party.emit('shoutBack',shoutBack); // send a chat to everyone connected to chat on this game
     });
 
     socket.on('createParty', function(data){
       logg('createParty');
-      // create a new party instance and return the id of the party.
-      socket.emit('createdParty',{party:'fun town'});
+      let partyId = partyIdTicker + "_" +  generateToken({});
+      let partyName = data.partyName;
+      let members = [];
+      members.push(partySockets[data.username]);
+      let leader = data.username;
+      let publicParty = data.publicParty; // if this party is visible in the public party list.
+
+      partyIdTicker++;
+      parties[partyId] = {partyId:partyId,partyName:partyName,publicParty:publicParty, members:members, leader:leader};
+      socket.emit('createdParty',{partyId:partyId,partyName:partyName,publicParty:publicParty}); // reply only to the socket that created the party.
+      // emit to everyone that there is a new part.
+      party.emit('newParty',{partyId:partyId,partyName:partyName,publicParty:publicParty});
     });
 
     socket.on('joinParty', function (data) {
       logg('joinParty');
       // validate its from a logged in user
-      // add them to the party if it exists.
      let shoutBack = {username:data.username}; // remove token basically.
-     party.emit('joinedParty',shoutBack); // send a chat to everyone connected to chat on this game
+     socket.emit('joinedParty',shoutBack); // let this socket know he is in the party.
+     // now update all of the other members of the same party that this new fella has joined.
     });
+
+    // invite player to party
+
+    // launching quest.
+
+    // vote on item
+
 
     socket.on('disconnect', (reason) => { // remove their socket reference
        // todo _s find out if they are in a party, if so remove them from it.
@@ -159,6 +179,9 @@ var tavern = io
 
   });
 
+
+//gameplaySocket
+// handle the actual in game actions? No, why build the socket group then?
 
 
 
